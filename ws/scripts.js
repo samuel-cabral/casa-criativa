0871b69fd913afae1fce0a58b70badae1c727c0a
Samuel Lima <samuel61lima@gmail.com>
2020-05-09 19:42:56 -0300

    This commit will finish the frontend.

--- ws/scripts.js
+++ ws/scripts.js
@@ -1,8 +1,16 @@
-document
-	.querySelector("button.fat")
-	.addEventListener("click", function(){
-		document
-			.querySelector("#footer")
-			.classList
-			.toggle("hide")
-	})
+function onOff() {
+	document
+		.querySelector("#modal")
+		.classList
+		.toggle("hide");
+
+	document
+		.querySelector("body")
+		.classList
+		.toggle("hideScroll");
+
+	document
+		.querySelector("#modal")
+		.classList
+		.toggle("addScroll");
+}
